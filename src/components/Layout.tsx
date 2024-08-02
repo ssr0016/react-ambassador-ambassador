4f6909fa216b387008cd9f505346450937585eab
ssr0016 <samsonrecluta0016@gmail.com>
2024-08-02 17:18:23 +0800

    first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Nav from './Nav';
+import Header from './Header';
+
+const Layout = (props : any) => {
+  return (
+    <div>
+      <Nav />
+      <main>
+        <Header />
+        <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            {props.children}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
